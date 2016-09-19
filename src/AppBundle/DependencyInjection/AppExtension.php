dd3969cb0f8043914b2c7be411a7ea79f7f0b835
Serj Bielanovskyi <kievswebdev@gmail.com>
2016-09-19 19:02:21 +0300

    OOT-1960: Import\Export abilities

--- src/AppBundle/DependencyInjection/AppExtension.php
+++ src/AppBundle/DependencyInjection/AppExtension.php
@@ -22,5 +22,6 @@ class AppExtension extends Extension
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('form.yml');
         $loader->load('services.yml');
+        $loader->load('importexport.yml');
     }
 }
